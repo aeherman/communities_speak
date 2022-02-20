f50b07228c492c997ab36aa5e4437ea678783ce4
Arielle <aeh2196@columbia.edu>
2022-02-20 14:38:02 -0500

    rm unnecessary files

--- code/names.R
+++
@@ -1,8 +0,0 @@
-library(googlesheets4)
-library(googledrive)
-library(tidyverse)
-
-var_dict <- gs4_find() %>% filter(name == "var_labels") %>% pull(id) %>%
-  read_sheet(sheet = "vars", na = c("na")) %>%
-  mutate(survey_q = as.character(survey_q),
-         qid = paste0("qid", q_no + 1))
