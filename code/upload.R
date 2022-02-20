202a36d14444f5478ec6a4ecebbdec8f47ca7502
Arielle <aeh2196@columbia.edu>
2022-02-20 14:33:53 -0500

    upload geoode visual

--- code/upload.R
+++ code/upload.R
@@ -6,3 +6,5 @@ googledrive_path <- "Communities Speak/Subteams/Data Subteam/Individual Survey-2
 drive_upload(media = paste0("~/communities_speak/data/codebook/codebook.csv"), path = paste0(googledrive_path, "data/output/codebook", today), type = "spreadsheet")
 drive_upload(media = paste0("../data/output/wrangled", today,".dta"), path = paste0(googledrive_path, "data/output/wrangled", today, ".dta"))
 drive_upload(media = paste0("../data/output/wrangled", today,".csv"), path = paste0(googledrive_path, "data/output/wrangled", today, ".csv"), type = "spreadsheet")
+
+#drive_upload(media = "~/communities_speak/code/geocode.pdf", path = paste0(googledrive_path, "visuals/geocode", today, ".pdf"))
