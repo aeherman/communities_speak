ae173a342b63638a555cf02e5c428aef01780c3b
Arielle <aeh2196@columbia.edu>
2022-02-22 03:34:46 -0500

    add wrangled and validation

--- code/upload.R
+++ code/upload.R
@@ -4,7 +4,9 @@ today <- gsub("-", "", Sys.Date())
 googledrive_path <- "Communities Speak/Subteams/Data Subteam/Individual Survey-2(POA/Codebook/Census Figures)/cleaning/"
 
 drive_upload(media = paste0("~/communities_speak/data/codebook/codebook.csv"), path = paste0(googledrive_path, "data/output/codebook", today), type = "spreadsheet")
-drive_upload(media = paste0("../data/output/wrangled", today,".dta"), path = paste0(googledrive_path, "data/output/wrangled", today, ".dta"))
-drive_upload(media = paste0("../data/output/wrangled", today,".csv"), path = paste0(googledrive_path, "data/output/wrangled", today, ".csv"), type = "spreadsheet")
+drive_upload(media = paste0("~/communities_speak/data/output/wrangled", today,".dta"), path = paste0(googledrive_path, "data/output/wrangled", today, ".dta"))
+drive_upload(media = paste0("~/communities_speak/data/output/wrangled", today,".csv"), path = paste0(googledrive_path, "data/output/wrangled", today, ".csv"), type = "spreadsheet")
+
+drive_upload(media = paste0("~/communities_speak/code/validation.pdf"), path = paste0(googledrive_path, "visuals/nonresponse", today, ".pdf"))
 
 #drive_upload(media = "~/communities_speak/code/geocode.pdf", path = paste0(googledrive_path, "visuals/geocode", today, ".pdf"))
