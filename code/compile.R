b090f50b173bdfe436c29fd8efc5e8f4d0a5fd1f
Arielle <aeh2196@columbia.edu>
2022-02-27 07:28:01 -0500

    unnest recordeddate

--- code/compile.R
+++ code/compile.R
@@ -7,33 +7,39 @@ ids <- gs4_find() %>% filter(str_detect(name, "Individual Survey")) %>% select(i
   mutate(name = str_replace_all(name, "Individual Survey- |_February.*", "")) %>%
   filter(name != "General Radio")
 
-survey <- map2(ids$id, ids$name, ~read_sheet(.x, sheet = "Sheet1",
+listed <- map2(ids$id, ids$name, ~read_sheet(.x, sheet = "Sheet1",
                                              na = c("NA", "", " ", "na", "Na", "n/a", "N/A", "N/a", "Not applicable")) %>%
-                 filter(row_number() != 1) %>%
-                 mutate_all(as.character) %>%
-                 mutate(source = .y)) %>%
-  reduce(bind_rows) %>% rename_all(str_to_lower) %>% mutate_all(str_to_lower) %>%
+                        filter(row_number() != 1) %>%
+                        mutate(source = .y) %>% 
+                        rename_all(str_to_lower) %>%
+                        mutate_at(vars(-recordeddate), ~str_to_lower(as.character(.))) %>%
+                        #unnest(recordeddate) %>%
+                        mutate_at(vars(recordeddate), as.list))
+
+survey <- listed %>% reduce(bind_rows) %>%
   rename(duration = "duration (in seconds)") %>%
-  dplyr::select(responseid, source, duration, recordeddate, contains("q"), -q44, -q45) %>% na_if("null") %>%
-  # gets rid of prolific id
-  mutate(q2 = ifelse(source == "prolific", "1", q2))
+  mutate(q2 = ifelse(source == "prolific", "1", q2)) %>%
+  dplyr::select(responseid, source, duration, recordeddate, progress, contains("q"), -q44, -q45) %>% na_if("null") %>%
+  unnest(recordeddate)
 
 general_radio_id <- gs4_find() %>% filter(str_detect(name, "General Radio")) %>% select(id, name) %>%
   mutate(name = str_replace_all(name, "Individual Survey- |_February.*", ""))
 
 general_radio <- read_sheet(general_radio_id$id, sheet = "Sheet1", na = c("NA", "", " ", "na", "Na", "n/a", "N/A", "N/a", "Not applicable")) %>%
-  filter(row_number() != 1) %>%
-  mutate_all(as.character) %>%
-  mutate(source = general_radio_id$name) %>%
-  rename_all(str_to_lower) %>% mutate_all(str_to_lower) %>%
+  filter(row_number() != 1) %>% mutate(source = general_radio_id$name) %>%
+  rename_all(str_to_lower) %>% mutate_at(vars(-recordeddate), ~str_to_lower(as.character(.))) %>%
   rename(duration = "duration (in seconds)") %>%
-  dplyr::select(responseid, source, duration, recordeddate, contains("q"), -q43, -q44) %>% na_if("null")
+  dplyr::select(responseid, source, duration, recordeddate, progress, contains("q"), -q43, -q44) %>% na_if("null") %>%
+  unnest(recordeddate)
 
 to_rename <- colnames(general_radio)[str_detect(colnames(general_radio), "[:digit:]")]
 replacement <- as.character(as.integer(str_extract(to_rename, "[:digit:]{1,2}")) + 1)
 new_names <- str_replace(to_rename, "[:digit:]{1,2}", replacement)
 names(general_radio[to_rename]) <- new_names
 names(general_radio)[match(to_rename, names(general_radio))] <- new_names
+
+#na_to_numeric <- general_radio[colSums(is.na(general_radio)) == 10] %>% select(!contains("text")) %>% mutate_all(as.numeric)
+#general_radio[colnames(na_to_numeric)] <- na_to_numeric
 
 survey <- survey %>% bind_rows(general_radio)
 
