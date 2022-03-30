2a2541685828a54aab7ac26cd4e3644cafb994a2
Arielle <aeh2196@columbia.edu>
2022-03-29 21:33:41 -0400

    na.omit, conf, show

--- code/functions/make_plots.R
+++ code/functions/make_plots.R
@@ -1,18 +1,15 @@
 library(tidyverse)
 
-df <- poa %>% mutate(inc_neg = inc_neg == 1)
-dems <- new_variables
-var <- c("emp_change", "inc_neg")
-item <- "inc_be_med_b"
-
-make_plots <- function(df, dems, var, min = 5, title = "Percent Newly Unemployed") {
+make_plots <- function(df, dems, var, min = 5, conf = 0.1,
+                       title = "Title", show = NULL) {
   #sym_var <- sym(var)
   out <- lapply(setNames(dems, dems), function(item){
     sym_item <- sym(item)
     reshaped <- df %>%
       group_by_at(vars(!!sym_item, var)) %>%
       count() %>%
-      group_by(!!sym_item) %>% mutate(prop = round(n/sum(n), digits = 4), denom = sum(n)) %>%
+      group_by(!!sym_item) %>% na.omit %>%
+      mutate(prop = round(n/sum(n), digits = 4), denom = sum(n)) %>%
       filter_if(is.logical, ~TRUE == .) %>%
       filter(!str_detect(!!sym_item, "prefer|;")) %>%
       ungroup %>% mutate(width = denom/sum(denom)) %>%
@@ -36,7 +33,7 @@ make_plots <- function(df, dems, var, min = 5, title = "Percent Newly Unemployed
         names(p.value) <- glue::glue("{cat} & {cat2} p-value:")
         if(any(
           glue::glue("{cat2} & {cat} p-value:") %in% names(p.values),
-          p.value > 0.1,
+          p.value > conf,
           is.na(p.value))) {
           next
         }
@@ -47,7 +44,7 @@ make_plots <- function(df, dems, var, min = 5, title = "Percent Newly Unemployed
     }
     
     # return plots that have at least on statistically significant value
-    if(is.null(p.values)) {
+    if(is.null(p.values) & is.null(show)) {
       return(NULL)
     } else {
       plot <- reshaped %>% filter(n >= min) %>%
@@ -65,17 +62,17 @@ make_plots <- function(df, dems, var, min = 5, title = "Percent Newly Unemployed
         xlab(NULL) + ylab(NULL) +
         ggtitle(glue::glue("{stringr::str_to_title(title)}\nby {stringr::str_to_title(item)}")) +
         labs(subtitle = paste(names(p.values), signif(p.values, 2), collapse = "\n")) +
-        geom_text(aes(label = glue::glue("{scales::percent(prop)}\n{n}/{denom}")),
+        geom_text(aes(label = glue::glue("{scales::percent(signif(prop, 4))}\n{n}/{denom}")),
                   color = project_pal[1], hjust = 1.2) 
       
         if(min(reshaped$n) < min) {
-          pulled <- reshaped %>% filter(n < min_exclude) %>%
+          pulled <- reshaped %>% filter(n < min) %>%
             mutate_if(labelled::is.labelled, labelled::to_character) %>%
             pull(!!sym_item)
           cats <- glue::glue("'{pulled}'") %>%
             paste(collapse = ", ")
           plot <- plot +
-            labs(caption = glue::glue("*Categories with fewer than {min_exclude} responses excluded: '{cats}'"))
+            labs(caption = glue::glue("*Categories with fewer than {min} responses excluded: '{cats}'"))
         }
       return(plot)
     }
