f461396e05eb00f90a0ca78c34f0957ce389a9bb
Arielle <aeh2196@columbia.edu>
2022-04-08 23:26:15 -0400

    add make_plots and weights

--- code/upload.R
+++ code/upload.R
@@ -3,7 +3,10 @@ today <- gsub("-", "", Sys.Date())
 
 googledrive_path <- "Communities Speak/Subteams/Data Subteam/Individual Survey-2(POA/Codebook/Census Figures)/cleaning/"
 
+drive_upload(media = paste0("~/communities_speak/code/functions/make_plots.R"), path = paste0(googledrive_path, "code/functions/make_plots", today), overwrite = TRUE)
+
 drive_upload(media = paste0("~/communities_speak/data/codebook/codebook.csv"), path = paste0(googledrive_path, "data/output/codebook", today), type = "spreadsheet")
+drive_upload(media = paste0("~/communities_speak/data/output/weights", today, ".csv"), path = paste0(googledrive_path, "data/output/weights", today), type = "spreadsheet")
 drive_upload(media = paste0("~/communities_speak/data/output/wrangled", today,".dta"), path = paste0(googledrive_path, "data/output/wrangled", today, ".dta"))
 drive_upload(media = paste0("~/communities_speak/data/output/wrangled", today,".rds"), path = paste0(googledrive_path, "data/output/wrangled", today, ".rds"))
 drive_upload(media = paste0("~/communities_speak/data/output/wrangled", today,".csv"), path = paste0(googledrive_path, "data/output/wrangled", today, ".csv"), type = "spreadsheet")
