6035af77c0a0674e289369fad476269261df7f8c
Arielle <aeh2196@columbia.edu>
2022-04-14 18:38:49 -0400

    set up sp22

---
+++ sp22/code/clean_data.R
@@ -0,0 +1,72 @@
+clean_data <- function(df = survey, col = NULL) {
+  by_col <- lapply(setdiff(colnames(df), c("responseid", "recordeddate")), function(col) {
+    #for (col in colnames(survey)[-1]) {
+    index <- which(survey_codebook$q == col)
+    values <- as.integer(unlist(stringr::str_split(survey_codebook$options[index], pattern = "[:punct:]")))
+    tags <- unlist(str_split(survey_codebook$choices[index], "; "))
+    
+    recode_na <- str_detect(tags, "not applicable")
+    
+    if(all(all.equal(values, c(1,2)) == TRUE, !is.na(recode_na) & recode_na)) {
+      df[[col]] <- 2 - as.integer(df[[col]] %>%
+                                    # hard code 3 - not applicable
+                                    na_if("3"))
+      values <- (2 - values) %>% na_if(-1)
+      
+    }
+    
+    named <- setNames(values, tags) %>% na.omit
+    
+    if(col %in% c(likert, simple)) {
+      out <- df[c("responseid", col)] %>%
+        mutate_at(vars(col), ~labelled(as.integer(.), named))
+      
+      # multiple answer  
+    } else if(col %in% mavr){
+      
+      sym_col <- sym(col)
+      out <- df[c("responseid", col)] %>%
+        fastDummies::dummy_cols(col, split = ",", ignore_na = TRUE) %>%
+        tidytext::unnest_tokens(output = !!sym_col, token = "regex",
+                                input = col, pattern = ",") %>%
+        mutate_at(col, ~factor(as.integer(.), levels = values, labels = tags)) %>%
+        group_by(responseid) %>% mutate_at(col, ~paste(., collapse = ";")) %>%
+        distinct %>% na_if("NA")
+      
+      
+      # don't double count prefer not to answer
+      if(any(str_detect(tags, "prefer not to answer"))) {
+        pnta <- which(names(named) == "prefer not to answer")
+        pnta_col <- paste(c(col, pnta), collapse = "_")
+        out <- out %>% mutate_at(pnta_col,
+                                 funs(ifelse(str_detect(!!sym_col, "prefer not to answer;|;prefer not to answer"),
+                                             0, !!sym(pnta_col)))) %>%
+          mutate_at(col, ~str_replace(., "prefer not to answer;|;prefer not to answer", ""))
+      }
+      
+      # haven label dummies
+      cols_to_label <- out %>% ungroup %>% select_if(is.numeric) %>% colnames
+      relabelled <- lapply(cols_to_label, function(dummy){
+        i <- as.integer(str_replace(dummy, paste0(col, "_"), ""))
+        values <- c(0, 1)
+        names(values) <- c(paste("not", tags[i]), tags[i])
+        out %>% ungroup %>% transmute_at(dummy, ~labelled(as.integer(.), values))
+      }) %>% reduce(bind_cols)
+      
+      out[cols_to_label] <- relabelled
+      
+      #r_zaiyctblk6t1z33
+    } else if(all(!str_detect(df[[col]], "[:alpha:]"), na.rm = TRUE) & any(!is.na(df[[col]]))) {
+      out <- df[c("responseid", col)] %>% mutate_at(vars(col), as.integer)
+    } else {
+      out <- df[c("responseid", col)]  
+    }
+    
+    # print(col)   
+    #}    
+    return(out)
+  })
+  
+  by_col %>% reduce(full_join, by = c("responseid"))
+  
+}
---
+++ sp22/code/default_setup.R
@@ -0,0 +1,44 @@
+# Default Setup for POA
+setwd("~/communities_speak/code/")
+library("tidyverse")
+library("labelled")
+source("../code/functions/communities_speak_theme.R")
+source("../code/functions/make_plots.R")
+#poa <- readRDS("../data/output/poa.rds")
+#wrangled <- readRDS("../data/output/wrangled20220410.rds")
+
+# thresholds
+min_duration <- 100 # 100 seconds
+min_completion <- .5 # responded to 50% of the questions
+poverty_line <- 5
+median_inc <- 8 # median income level is $67
+
+# census numbers
+## only necessary to make city-wide estimates
+## unideal for weight to be greater than 2 or 3
+census <- tribble(~race, ~target,
+                  "asian", .14,
+                  "black or african american", .241,
+                  "hispanic or latinx", .291,
+                  "white (non-hispanic or latino)", .321,
+                  "american indian or alaska native", .01,
+                  "hawaiin or pacific islander", .001,
+                  "two or more races", .027) #%>%
+  #mutate(col_name = glue::glue("race_{race}")) %>% arrange(race)
+
+demographics <-
+  c("borough", # a
+    "decade", # b (from poa data)
+    "gen", # c
+    "race_census", # d
+    "not_eng", # e
+    "mar", # f
+    "sch_level_cat", # g
+    "hh_ch_0_17_bi", # h
+    "hh_65_bi", # i
+    "inc_dist", # j figure out if it is past or present
+    "emp_status_before", # k
+    "emp_status_after", # k
+    "res_cat" # l
+    )
+names(demographics) <- demographics
