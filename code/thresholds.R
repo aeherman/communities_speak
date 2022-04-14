debbaa625dd6b0c2abef2254dac550b52d0df43f
Arielle <aeh2196@columbia.edu>
2022-04-14 18:40:00 -0400

    extract thresholds and census data

---
+++ code/thresholds.R
@@ -0,0 +1,14 @@
+# thresholds
+min_duration <- 100 # 100 seconds
+min_completion <- .5 # responded to 50% of the questions
+poverty_line <- 5
+median_inc <- 8 # median income level is $67K
+
+census <- tribble(~race, ~target,
+                  "asian", .14,
+                  "black or african american", .241,
+                  "hispanic or latinx", .291,
+                  "white (non-hispanic or latino)", .321,
+                  "american indian or alaska native", .01,
+                  "hawaiin or pacific islander", .001,
+                  "two or more races", .027)
