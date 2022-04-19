b0dfc3ab3da0b6806fb4d1eca5ec0b199cd32833
Arielle <aeh2196@columbia.edu>
2022-04-18 23:16:34 -0400

    update google path fa21

--- code/upload.R
+++ code/upload.R
@@ -1,11 +1,11 @@
 # upload
 today <- gsub("-", "", Sys.Date())
 
-googledrive_path <- "Communities Speak/Subteams/Data Subteam/Individual Survey-2(POA/Codebook/Census Figures)/cleaning/"
+googledrive_path <- "Communities Speak/Subteams/Data Subteam/cleaning/fa21/"
 
 drive_upload(media = paste0("~/communities_speak/code/functions/make_plots.R"), path = paste0(googledrive_path, "code/functions/make_plots", today), overwrite = TRUE)
 
-drive_upload(media = paste0("~/communities_speak/data/codebook/codebook.csv"), path = paste0(googledrive_path, "data/output/codebook", today), type = "spreadsheet")
+drive_upload(media = glue::glue("~/communities_speak/data/codebook/codebook{today}.csv"), path = paste0(googledrive_path, "data/output/codebook", today), type = "spreadsheet")
 drive_upload(media = paste0("~/communities_speak/data/output/weights", today, ".csv"), path = paste0(googledrive_path, "data/output/weights", today), type = "spreadsheet")
 drive_upload(media = paste0("~/communities_speak/data/output/wrangled", today,".dta"), path = paste0(googledrive_path, "data/output/wrangled", today, ".dta"))
 drive_upload(media = paste0("~/communities_speak/data/output/wrangled", today,".rds"), path = paste0(googledrive_path, "data/output/wrangled", today, ".rds"))
