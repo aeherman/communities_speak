a465255a99cd8a93f4ef8b900ce1a69fa31673e7
Arielle <aeh2196@columbia.edu>
2022-04-22 05:33:11 -0400

    describe income category levels

--- code/thresholds.R
+++ code/thresholds.R
@@ -1,8 +1,8 @@
 # thresholds
 min_duration <- 100 # 100 seconds
 min_completion <- .5 # responded to 50% of the questions
-poverty_line <- 5
-median_inc <- 8 # median income level is $67K
+poverty_line <- 5 # poverty level is $37K: at or below poverty line
+median_inc <- 8 # median income level is $67K: at or below median income level
 
 census <- tribble(~race, ~target,
                   "asian", .14,
