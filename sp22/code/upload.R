8f359fd85292d1eab88eb66b9fcf98617cdc3dd2
Arielle <aeh2196@columbia.edu>
2022-05-22 05:22:33 -0400

    add project pathway

--- sp22/code/upload.R
+++ sp22/code/upload.R
@@ -1,14 +1,16 @@
 # upload
+
 today <- gsub("-", "", Sys.Date())
 
 googledrive_path <- "Communities Speak/Subteams/Data Subteam/cleaning/sp22/"
 project_path <- "~/communities_speak/sp22/"
 
+
 #drive_upload(media = paste0("~/communities_speak/code/functions/make_plots.R"), path = paste0(googledrive_path, "code/functions/make_plots", today), overwrite = TRUE)
 
 # upload data/processed
 
-lapply(list.files("data/processed", full.names = TRUE), function(file) {
+lapply(list.files(glue::glue("{project_path}data/processed"), full.names = TRUE), function(file) {
   split <- unlist(str_split(file, pattern = "[:punct:]"))
   name <- nth(split, -2)
   ending <- last(split)
@@ -25,7 +27,7 @@ lapply(list.files("data/processed", full.names = TRUE), function(file) {
 
 # upload data/output
 
-lapply(list.files("data/output", full.names = TRUE), function(file) {
+lapply(list.files(glue::glue("{project_path}data/output"), full.names = TRUE), function(file) {
   split <- unlist(str_split(file, pattern = "[:punct:]"))
   name <- nth(split, -2)
   ending <- last(split)
@@ -42,9 +44,9 @@ lapply(list.files("data/output", full.names = TRUE), function(file) {
 
 # upload code/functions
 
-lapply(list.files("code/functions", full.names = TRUE), function(file){
+lapply(list.files(glue::glue("{project_path}code/functions"), full.names = TRUE), function(file){
   partial <- last(unlist(str_split(file, pattern = "/")))
   drive_upload(media = file, path = glue::glue("{googledrive_path}code/functions/{partial}"), overwrite = TRUE) 
 })
 
-drive_upload(media = "code/cleaning.Rmd", path = glue::glue("{googledrive_path}code/cleaning{today}.Rmd"), overwrite = TRUE) 
+drive_upload(media = glue::glue("{project_path}code/cleaning.Rmd"), path = glue::glue("{googledrive_path}code/cleaning{today}.Rmd"), overwrite = TRUE) 
