4dd4a849b3bb2a5a87533428cb39aa3ec76efdd5
Arielle <aeh2196@columbia.edu>
2022-05-24 12:37:11 -0400

    rm old weight definitions

--- sp22/code/default_setup.R
+++ sp22/code/default_setup.R
@@ -15,20 +15,6 @@ median_inc <- 3 # median income level is 69.5K (top of category 3)
 median_inc_val <- 69500
 
 
-# census numbers
-# https://www.census.gov/quickfacts/fact/table/newyorkcitynewyork,US/PST045221
-## only necessary to make city-wide estimates
-## unideal for weight to be greater than 2 or 3
-census <- tribble(~race, ~target,
-                  "asian", .14,
-                  "black or african american", .241,
-                  "hispanic or latinx", .291,
-                  "white (non-hispanic or latino)", .321,
-                  "american indian or alaska native", .01,
-                  "hawaiin or pacific islander", .001,
-                  "two or more races", .027) #%>%
-  #mutate(col_name = glue::glue("race_{race}")) %>% arrange(race)
-
 demographics <-
   c("borough", # a
     "decade", # b (from poa data)
