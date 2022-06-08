b983fa79c032b721451dbfc89f6dcc699c1c4e56
Arielle <aeh2196@columbia.edu>
2022-06-08 07:16:00 -0400

    add language fix from boroughs.R

--- sp22/code/tracker_fix.R
+++ sp22/code/tracker_fix.R
@@ -6,5 +6,10 @@ source("code/default_setup.R")
 
 tracker_id <- gs4_find() %>% filter(name == "tracker_sp22") %>% pull(id)
 
+# fix emerson tracking field
 write_sheet(final_clean %>% filter(is.na(test_response)) %>% select(responseid, aid),
             tracker_id, "eme_fix")
+
+# fix q language after adding official embedded data field in later
+#write_sheet(final_clean %>% filter(is.na(test_response)) %>% select(userlanguage),
+#            borough_id, "language_fix")
