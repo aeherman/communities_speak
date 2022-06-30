11bd199ed1cd0246c7d1fd686e77416add940c62
Arielle <aeh2196@columbia.edu>
2022-06-30 05:17:54 -0400

    modularize upload

---
+++ sp22/code/functions/upload_folder.R
@@ -0,0 +1,43 @@
+upload_folder <- function(googledrive_path = "Communities Speak/Subteams/Data Subteam/cleaning/sp22/",
+                          project_path = "~/communities_speak/sp22/",
+                          today = gsub("-", "", Sys.Date()),
+                          pattern. = NULL,
+                          file_path)
+  {
+  
+  # error handling
+  if(str_sub(file_path, -1) == "/") {
+    message("File path must point to a folder without slash '/'")
+    stop(str_sub(file_path, -1) == "/")
+  }
+  
+  files <- list.files(glue::glue("{project_path}{file_path}"), full.names = TRUE)
+  
+  if(!is.null(pattern.)) {
+    files <- grep(files, pattern = pattern., value = TRUE)
+  }
+  
+  lapply(files, function(file) {
+    split <- unlist(stringr::str_split(file, pattern = "\\.|\\/"))
+    name <- str_replace_all(dplyr::nth(split, -2),
+                            c("survey_codebook_" = ""))
+    ending <- dplyr::last(split)
+    modified <- stringr::str_replace_all(as.Date(file.info(file)$mtime), "-", "")
+    
+    spreadsheet <- NULL
+    if(ending == "csv") {
+      spreadsheet <- "spreadsheet"
+    }
+    
+    if(today > modified) {
+      message(glue::glue("{name} last modified on {modified}."))
+      return(NULL)
+    } else {
+      drive_upload(media = file,
+                   path = glue::glue("{googledrive_path}{file_path}/{name}{today}.{ending}"),
+                   type = spreadsheet)
+    }
+    
+  })
+  
+}
--- sp22/code/upload.R
+++ sp22/code/upload.R
@@ -1,70 +1,25 @@
 # upload
 library(googledrive)
 library(googlesheets4)
-today <- gsub("-", "", Sys.Date())
 
+source("functions/upload_folder.R")
+
+upload_folder(file_path = "data/processed")
+upload_folder(file_path = "data/output")
+upload_folder(file_path = "reports/poa", pattern. = "pdf")
+
+today <- gsub("-", "", Sys.Date())
 googledrive_path <- "Communities Speak/Subteams/Data Subteam/cleaning/sp22/"
 project_path <- "~/communities_speak/sp22/"
 
-#drive_upload(media = paste0("~/communities_speak/code/functions/make_plots.R"), path = paste0(googledrive_path, "code/functions/make_plots", today), overwrite = TRUE)
-
-# upload data/processed
-
-lapply(list.files(glue::glue("{project_path}data/processed"), full.names = TRUE), function(file) {
-  split <- unlist(str_split(file, pattern = "[:punct:]"))
-  name <- nth(split, -2)
-  ending <- last(split)
-  
-  spreadsheet <- NULL
-  if(ending == "csv") {
-    spreadsheet <- "spreadsheet"
-  }
-  
-  drive_upload(media = file, path = glue::glue("{googledrive_path}data/processed/{name}{today}.{ending}"),
-               type = spreadsheet)
-  
-})
-
-# upload data/output
-
-lapply(list.files(glue::glue("{project_path}data/output"), full.names = TRUE), function(file) {
-  split <- unlist(str_split(file, pattern = "[:punct:]"))
-  name <- nth(split, -2)
-  ending <- last(split)
-  
-  if(ending == "csv") {
-    spreadsheet <- "spreadsheet"
-  } else {
-    spreadsheet <- NULL
-  }
-  
-  drive_upload(media = file, path = glue::glue("{googledrive_path}data/output/{name}{today}.{ending}"),
-               type = spreadsheet)
-})
-
 # upload code/functions
-
 lapply(list.files(glue::glue("{project_path}code/functions"), full.names = TRUE), function(file){
   partial <- last(unlist(str_split(file, pattern = "/")))
   drive_upload(media = file, path = glue::glue("{googledrive_path}code/functions/{partial}"), overwrite = TRUE) 
 })
 
-# upload POAs
-lapply(grep(list.files(glue::glue("{project_path}code/poa")), pattern = "pdf", value = TRUE),
-       function(file) {
-         full_name <- glue::glue("{project_path}code/poa/{file}")
-         modified <- str_replace_all(as.Date(file.info(full_name)$mtime), "-", "")
-         
-         if(today == modified) {
-           return(NULL)
-         } else {
-           pre <- unlist(strsplit(file, "\\."))[1]
-           drive_upload(media = glue::glue(full_name),
-                        path = glue::glue("{googledrive_path}reports/poa/{pre}{today}.pdf"),
-                        overwrite = overwrite)
-         }
-       })
-
-drive_upload(media = glue::glue("{project_path}code/cleaning.Rmd"), path = glue::glue("{googledrive_path}code/cleaning{today}.Rmd"), overwrite = TRUE)
+# upload specific files
+drive_upload(media = glue::glue("{project_path}code/cleaning.Rmd"),
+             path = glue::glue("{googledrive_path}code/cleaning{today}.Rmd"), overwrite = TRUE)
 drive_upload(media = glue::glue("{project_path}reports/emerson_demographics.pdf"), path = glue::glue("{googledrive_path}reports/panel_vs_online{today}.pdf"), overwrite = TRUE)
 
