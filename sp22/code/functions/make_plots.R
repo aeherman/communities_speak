6461a4cb6be51ea1dd88a986eb8d92386f3e5f2a
Arielle <aeh2196@columbia.edu>
2022-07-14 16:31:12 -0400

    just fill, not color; reverse legend; hyp_var_label; add legend to original plot command

--- sp22/code/functions/make_plots.R
+++ sp22/code/functions/make_plots.R
@@ -8,10 +8,15 @@ library(tidyverse)
 # title: the title of the plo
 # show: currently nonfunctional argument that I plan to use to allow more flexibility in showng plots with nonsignificant relationships
 
-
 # by_vars -> "by"
 make_plots <- function(df, by_vars, hyp_var, min = 5, conf = 0.01,
-                       title = "Title", show = NULL) {
+                       title = "Title", show = NULL,
+                       codebook = read_csv(file.path(root, "data/output/codebook.csv"),
+                                           show_col_types = FALSE)) {
+  
+  
+  hyp_var_labels <- codebook %>% filter(variable %in% hyp_var) %>% select(variable, description) %>%
+    mutate(hyp_var_label = glue::glue("{variable}: {description}")) %>% select(variable, hyp_var_label)
   
   #sym_var <- sym(hyp_var)
   
@@ -30,11 +35,15 @@ make_plots <- function(df, by_vars, hyp_var, min = 5, conf = 0.01,
       mutate(stat = str_replace_all(stat, c("1" = "n", "2" = "denom", "3" = "prop"))) %>%
       pivot_wider(id_cols = c(!!sym_item, "hyp_var"), names_from = stat) %>%
       na.omit %>%
+      # translate numbers to labels
       mutate_if(labelled::is.labelled, labelled::to_character) %>%
+      # replace long labels with readable shorter values
       mutate(across(!!sym_item,
                     ~str_replace_all(.,
                                     c("transgender.*" = "transgender",
-                                      "indigenous.*" = "Indigenous American")))) %>%
+                                      "indigenous.*" = "Indigenous American")))
+             ) %>%
+      left_join(hyp_var_labels, by = c("hyp_var" = "variable")) %>%
       arrange(hyp_var, prop)
     
     filtered <- reshaped %>% filter(if_all(starts_with("denom"), ~.>=min)) #%>% select(-!!sym_var, -width)
@@ -85,6 +94,10 @@ make_plots <- function(df, by_vars, hyp_var, min = 5, conf = 0.01,
       return(p.values_table)
     })
     
+    legend_labels_df <- codebook %>% filter(variable %in% hyp_var) %>% select(variable, description)
+    legend_labels <- legend_labels_df$description[match(hyp_var, legend_labels_df$variable)]
+    hyp_legend_labels <- sort(paste(hyp_var, legend_labels, sep = ": "))
+    
     # return plots that have at least one statistically significant value
     if(all(is.na(unlist(p.values))) & is.null(show) | is.null(p.values) & is.null(show) & length(hyp_var) == 1) {
       return(NULL)
@@ -94,9 +107,8 @@ make_plots <- function(df, by_vars, hyp_var, min = 5, conf = 0.01,
         ggplot(aes(x = prop,
                    y = reorder(stringr::str_to_title(labelled::to_character(!!sym_item)),
                                -prop),
-                   group = hyp_var,
-                   fill = hyp_var,
-                   color = hyp_var)) +
+                   group = hyp_var_label,
+                   fill = hyp_var_label)) +
         geom_col(position = "dodge") +
         geom_text(aes(label = glue::glue("{signif(prop, 2)*100}% {n}/{denom}")),
                   # vertical
@@ -116,21 +128,21 @@ make_plots <- function(df, by_vars, hyp_var, min = 5, conf = 0.01,
         
         scale_x_continuous(labels = scales::percent, limits = c(0,1)) +
         scale_fill_manual(NULL, values = project_pal) +
-        scale_color_manual(NULL, values = project_pal) +
+        #scale_color_manual(NULL, values = project_pal) +
         project_theme + # question: add a variable for this?
         
         # labels
         xlab(NULL) + ylab(NULL) +
         ggtitle(glue::glue("{stringr::str_to_title(title)} by {stringr::str_to_title(item)}")) +
-        #annotation_custom(grob = grid::textGrob(paste(names(p.values), p.values, collapse = "\n")),  
-        #                  xmin = 2, xmax = 2, ymin = -4.5, ymax = -10) +
-        #labs(subtitle = paste(names(p.values), p.values, collapse = "\n")) +
+        
         theme(
           plot.subtitle = element_text(size = 8),
           plot.title = element_text(size = 10),
           plot.caption = element_text(size = 7),
-          axis.text.x = element_text(size = 8)
-        )
+          axis.text.x = element_text(size = 8),
+          legend.position = "bottom",
+          legend.direction = "vertical"
+        ) + guides(fill = guide_legend(reverse = TRUE))
         
         if(min(reshaped$denom) < min) {
           pulled <- reshaped %>% filter(denom < min) %>%
@@ -148,9 +160,6 @@ make_plots <- function(df, by_vars, hyp_var, min = 5, conf = 0.01,
       
       if(length(hyp_var) == 1) {
         plot <- plot + theme(legend.position = "none")
-      } else {
-        plot <- plot + theme(legend.position = "bottom") #+
-          #guides(fill = guide_legend(ncol = 1))
       }
         
     }
