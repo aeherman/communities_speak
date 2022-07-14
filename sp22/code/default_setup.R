d200437a836fcc10951df852bd9c471f527ffc3b
Arielle <aeh2196@columbia.edu>
2022-07-14 18:15:18 -0400

    extract loading of codebook to default_setup

--- sp22/code/default_setup.R
+++ sp22/code/default_setup.R
@@ -11,9 +11,10 @@ source(file.path(root, "code/communities_speak_theme.R"))
 source(file.path(root, "code/functions/make_plots.R"))
 
 # data
+codebook <- read_csv(file.path(root, "data/output/codebook.csv"), show_col_types = FALSE)
+load(file.path(root, "data/processed/survey_codebook_labeled.rdata"))
 wrangled <- readRDS(file.path(root, "data/output/wrangled.rds"))
 load(file.path(root, "data/processed/cleaned.rdata"))
-load(file.path(root, "data/processed/survey_codebook_labeled.rdata"))
 
 # thresholds
 min_duration <- 100 # 100 seconds
